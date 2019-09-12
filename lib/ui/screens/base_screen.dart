493463c3f1ea8fb9c55a63d14dbb10c395459f31
Enzo Lizama <lizama.enzo@gmail.com>
2019-09-11 23:49:49 -0500

    base screen to get inhiterance

--- lib/ui/screens/base_screen.dart
+++ lib/ui/screens/base_screen.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_di/core/viewmodels/base_view_model.dart';
+import 'package:provider_di/locator.dart';
 
-class BaseScreen extends StatefulWidget {
+class BaseScreen<T extends BaseViewModel> extends StatefulWidget {
+  final Widget Function(BuildContext context, T model, Widget child) builder;
+  final Function(T) onModelReady;
+
+  const BaseScreen({Key key, this.builder, this.onModelReady})
+      : super(key: key);
+
   @override
-  _BaseScreenState createState() => _BaseScreenState();
+  _BaseScreenState<T> createState() => _BaseScreenState<T>();
 }
 
-class _BaseScreenState extends State<BaseScreen> {
+class _BaseScreenState<T extends BaseViewModel> extends State<BaseScreen<T>> {
+  T model = locator<T>();
+
+  @override
+  void initState() {
+    if (widget.onModelReady != null) widget.onModelReady(model);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return ChangeNotifierProvider<T>(
+      builder: (context) => model,
+      child: Consumer<T>(
+        builder: widget.builder,
+      ),
     );
   }
 }
