d424cfa7ac7b90f55e8c6e45e4e7d33ef632da33
Enzo Lizama <lizama.enzo@gmail.com>
2019-09-12 09:58:39 -0500

    main setup of provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider_di/ui/screens/login_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_di/core/models/user.dart';
+import 'package:provider_di/core/services/authentication.dart';
+import 'package:provider_di/locator.dart';
+import 'package:provider_di/routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,12 +11,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return StreamProvider<User>(
+      initialData: User.initial(),
+      builder: (context) => locator<AuthenticationService>().userController,
+      child: MaterialApp(
+        title: 'Provider Architecture Example',
+        theme: ThemeData(),
+        initialRoute: initialRoute,
+        onGenerateRoute: Router.generateRoutes,
       ),
-      home: LoginScreen()
     );
   }
 }
