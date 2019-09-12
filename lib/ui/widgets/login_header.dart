52de6ea25dc6f90ec2e177a371368cb93cbf0c9e
Enzo Lizama <lizama.enzo@gmail.com>
2019-09-12 09:58:20 -0500

    update widgets ui

--- lib/ui/widgets/login_header.dart
+++ lib/ui/widgets/login_header.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:provider_di/ui/commons/text_styles.dart';
+import 'package:provider_di/ui/commons/ui_helpers.dart';
 
 class LoginHeader extends StatelessWidget {
+  final TextEditingController controller;
+  final String validationMessage;
+
+  const LoginHeader(
+      {Key key, @required this.controller, this.validationMessage})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('Login', style: headerStyle),
+        UIHelper.verticalSpaceMedium(),
+        Text('Enter a number between 1 - 10', style: subHeaderStyle),
+        LoginTextField(controller: controller),
+        this.validationMessage != null
+            ? Text(validationMessage, style: TextStyle(color: Colors.red))
+            : Container()
+      ],
+    );
+  }
+}
+
+class LoginTextField extends StatelessWidget {
+  final TextEditingController controller;
+
+  const LoginTextField({Key key, this.controller}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      padding: EdgeInsets.symmetric(horizontal: 15.0),
+      margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+      height: 50.0,
+      alignment: Alignment.centerLeft,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: TextField(
+        decoration: InputDecoration.collapsed(hintText: 'User id'),
+        controller: controller,
+      ),
     );
   }
 }
